9b6a11e7a034d3b7f2e548f8ac206643fbd1a6ee
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-22 20:41:35 +0100

    Weather App - JSX

---
+++ src/components/weather/Weather.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Weather'
+
+const Weather = () => {
+  return (
+    <section className='-100vh --center-all'>
+        <div className="container weather --flex-center">
+            <div className="weather-app --text-light">
+                <h1>Weather App</h1>
+                <p>22-01-2024</p>
+                <div className="--form-control --my2">
+                    <input type="text" placeholder='Search city name' />
+                </div>
+                <div className="result --card --my2">
+                    <h2>Libreville</h2>
+                    <img src="" alt="Clouds" />
+                    <p>Temperature : 23°C</p>
+                    <p>Weather : Clouds</p>
+                    <p>Temperature range : 18°C - 25°C</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Weather;
