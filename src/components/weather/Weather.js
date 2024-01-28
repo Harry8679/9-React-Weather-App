6640250c2789aef0384236f8519c7d41571f7738
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-28 01:11:38 +0100

    Weather App - Logic 3: Loading State and Handling Error

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import './Weather.scss';
 
 const Weather = () => {
+  const today = new Date();
+  const month = today.getMonth() + 1
+  let date = today.getDate() + '/' +(month < 10 ? (0 + '' + month): month) + '/' +today.getFullYear();
   const [input, setInput] = useState('');
   const [weather, setWeather] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(true);
   const [errorMsg, setErrorMsg] = useState('');
+  const [dateToday, setDayToday] = useState(date);
 
   const api = {
     url: 'https://api.openweathermap.org/data/2.5/',
@@ -26,13 +30,27 @@ const Weather = () => {
     }
 
     if (e.key === 'Enter' && input !== '') {
+        setIsLoading(true);
+        setError(true);
         fetch(`${api.url}weather?q=${input}&unit=metrics&APPID=${api.key}`)
-        .then((res) => res.json())
+        .then((res) => {
+            if (!res.ok) {
+                throw Error('Failed to Fetch Data');
+            }
+            return res.json();
+        })
         .then((data) => {
             console.log(data);
             setWeather(data);
             setInput('');
             setError(false);
+            setIsLoading(false);
+        })
+        .catch((err) => {
+            console.log(err.message);
+            setError(true);
+            setErrorMsg(err.message);
+            setIsLoading(false);
         });
     }
   }
@@ -42,21 +60,22 @@ const Weather = () => {
         <div className="container weather --flex-center">
             <div className="weather-app --text-light">
                 <h1>Weather App</h1>
-                <p>22-01-2024</p>
+                <p>{dateToday}</p>
                 <div className="--form-control --my2">
                     <input type="text" placeholder='Search city name' onChange={getInput} value={input} onKeyDown={getWeatherData} />
                 </div>
                 {error ? (
-                    <p>{errorMsg}</p>
+                    <p className={errorMsg !== '' && 'error'}>{errorMsg}</p>
                 ):(
                     <div className="result --card --my2">
                         <h2>{weather.name}, {weather.sys.country}</h2>
                         <img src={iconUrl + weather.weather[0].icon+'.png'} alt={weather.weather[0].main} />
-                        <p>Temperature : {Math.round(weather.main.temp)}°C</p>
+                        <p>Temperature : {Math.round(weather.main.temp - 273.15)}°C</p>
                         <p>Weather : {weather.weather[0].main}</p>
-                        <p>Temperature range : {Math.round(weather.main.temp_min)}°C - {Math.round(weather.main.temp_max)}°C</p>
+                        <p>Temperature range : {Math.round(weather.main.temp_min - 273.15)}°C - {Math.round(weather.main.temp_max - 273.15)}°C</p>
                     </div>
                 )}
+                {isLoading && <h3>Loading</h3>}
             </div>
         </div>
     </section>
